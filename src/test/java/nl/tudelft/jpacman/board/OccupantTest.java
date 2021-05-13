5100aa783541fe2d6eab009b7bc8ce8e708a7c20
WANGZHE <1103270775@qq.com>
2021-05-13 16:41:33 +0800

    Initial commit

---
+++ src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
@@ -0,0 +1,68 @@
+package nl.tudelft.jpacman.board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
+
+/**
+ * Test suite to confirm that {@link Unit}s correctly (de)occupy squares.
+ *
+ * @author Jeroen Roosen
+ *
+ */
+class OccupantTest {
+
+    /**
+     * The unit under test.
+     */
+    private Unit unit;
+
+    private Square square;
+
+    /**
+     * Resets the unit under test.
+     */
+    @BeforeEach
+    void setUp() {
+        unit = new BasicUnit();
+    }
+
+    /**
+     * Asserts that a unit has no square to start with.
+     */
+    @Test
+    void noStartSquare() {
+        // Remove the following placeholder:
+        assertThat(unit.hasSquare()).isFalse();
+    }
+
+    /**
+     * Tests that the unit indeed has the target square as its base after
+     * occupation.
+     */
+    @Test
+    void testOccupy() {
+        // Remove the following placeholder:
+        Square target = new BasicSquare();
+        unit.occupy(target);
+        assertThat(unit.getSquare()).isEqualTo(target);
+        assertThat(target.getOccupants()).contains(unit);
+    }
+
+    /**
+     * Test that the unit indeed has the target square as its base after
+     * double occupation.
+     */
+    @Test
+    void testReoccupy() {
+        // Remove the following placeholder:
+        Square target = new BasicSquare();
+        unit.occupy(target);
+        unit.occupy(target);
+        assertThat(unit.getSquare()).isEqualTo(target);
+        assertThat(target.getOccupants()).contains(unit);
+    }
+}
